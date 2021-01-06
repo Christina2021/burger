bd029a4198b41b17cc33d8a4b8917100f92279e5
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-01-05 21:18:29 -0800

    Update schema.sql with column devoured having DEFAULT false.

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,5 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN
+    devoured BOOLEAN DEFAULT false
 );
